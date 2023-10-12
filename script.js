7daf2cf7b668c57659f0a3ab85d89525b674f78e
Dugidem Egwuatu <Dugidemeg@gmail.com>
2023-10-12 23:19:44 +0100

    city name, humidity and windspeed details updated from API

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 const apiKey = "15e38657459adb88c161abea88905b94 ";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=lagos";
+
+async function checkWeather(){
+    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+    let data = await response.json();
+
+    console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temperature").innerHTML = data.main.temp + "&deg;C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+}
+
+checkWeather();
